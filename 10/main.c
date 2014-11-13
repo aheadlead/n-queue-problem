e1f163be0f0c46c9c73cdaf3f5d53ddc773e315a
aheadlead <aheadlead@dlifep.com>
2014-11-13 17:10:47 +0800

    遇到了一个奇怪的错误

--- 10/main.c
+++ 10/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #define MAXN 32
-#define MAX_THREAD_NUMBER 2
+#define MAX_THREAD_NUMBER 1
 int N;
 int solution_total={0};
 int upperlim;
@@ -18,6 +18,7 @@ struct task
 struct task task_queue[MAXN]={{{0, 0, 0}, 0}};
 size_t task_queue_length=0;
 pthread_t thread_pool[MAX_THREAD_NUMBER];
+pthread_attr_t attr;
 
 void test_non_recursive(int _row, int _ld, int _rd, int id)
 {
@@ -40,7 +41,7 @@ void test_non_recursive(int _row, int _ld, int _rd, int id)
         if (0 == *ppos)
         {
             if ((*prow) == upperlim)
-                task_queue[id].ans++;
+               task_queue[id].ans++;
             while (0 == *ppos)
             {
                 ppos--;
@@ -60,8 +61,33 @@ void test_non_recursive(int _row, int _ld, int _rd, int id)
 
 void* multithreading(void* _id)
 {
-    long long id=(long long)_id;
+    int id=(long long)_id;
     size_t i;
+
+    /* TODO 这里有一个奇怪的错误。
+     *
+     * 编译器: Apple LLVM version 6.0 (clang-600.0.54) (based on LLVM 3.5svn) 
+     * 系统: OS X 10.10 Yosemite
+     * 遇到问题时的编译参数: gcc main.c -O3 -g
+     *
+     * 当我引入了pthread多线程库，使得这个算法可以多线程运行后。
+     * 如果不加上下面这一行fprintf(stderr, " ");，
+     * 系统会提示"Bus error: 10"。
+     * 
+     * 事实上直接printf(" "); 却是能正常工作的（奇怪吧），
+     * 但是这样就会有一个空格输出到stdout，
+     * 而我的测试脚本（根目录下的run.py）依赖于stdout的结果。
+     * 
+     * 下面这行语句如果改为"printf("");"的话，问题依旧。
+     * 而且这行语句必须在这一行和这一行之前执行，否则会出现问题。
+     *
+     * 使用lldb对程序进行调试，因为开启了-O3参数，调试很蛋疼。
+     * （如果您有开了优化参数的调试经验，请不吝赐教。）
+     *
+     * aheadlead 2014年11月13日 星期四 17时10分20秒 CST
+     *
+     */
+    fprintf(stderr, " ");
 
     for (i=id; i<task_queue_length; i=i+MAX_THREAD_NUMBER)
     {
@@ -89,8 +115,10 @@ int main()
     if (N & 1)
         task_queue[task_queue_length++] = (struct task){{1<<(N/2), 1<<(N/2+1), 1<<(N/2-1)}, 0};
 
+    pthread_attr_init(&attr);
+    pthread_attr_setstacksize(&attr, 1*1048576);
     for (i=0; i<MAX_THREAD_NUMBER; ++i)
-        pthread_create(&thread_pool[i], NULL, multithreading, (void*)(long long)i);
+        pthread_create(&thread_pool[i], &attr, multithreading, (void*)(long long)i);
 
     for (i=0; i<MAX_THREAD_NUMBER; ++i)
         pthread_join(thread_pool[i], NULL);
